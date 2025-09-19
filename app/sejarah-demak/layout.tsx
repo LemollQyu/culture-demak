c06aee85b6c04809186f5b7e23c54b7737b8c0b4
unknown <lemoll@gmail.com>
2025-09-19 17:11:07 +0700

    hanya layouting sejarah demak

---
+++ app/sejarah-demak/layout.tsx
@@ -0,0 +1,13 @@
+export default function LayoutSejarahDemak({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+  
+    <div>
+        {children}
+    </div>
+  
+  );
+}
